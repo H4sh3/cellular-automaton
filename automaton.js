ac1e805231828e26b58636b94657c80045accc68
h4sh3 <h4sh3@git.com>
2020-09-12 17:14:36 +0200

    refactor

--- automaton.js
+++ automaton.js
@@ -1,20 +1,20 @@
-const state = {};
-let color;
-let uniqCenter;
-let worker;
+const state = {
+  color: 0,
+  uniqStates: new Set(),
+};
 
 let settings = {
   strokeOn: true,
   nRange: 1,
-  frameRate: getFrameRateFor(1),
-  size: 32,
-  rgbOffsets: [0,2,4],
+  framerate: 15,
+  size: 64,
+  rgbOffsets: [0, 2, 4],
   initColorOffsets: function () {
-    const x = [0];
+    this.rgbOffsets = [];
     for (let i = 0; i < 3; i++) {
-       x.push(Math.floor(random(0, 9)));
+      const v = Math.floor(Math.random() * Math.floor(9));
+      this.rgbOffsets.push(v);
     }
-    this.rgbOffsets = shuffle(x);
   },
   toggleStroke: function () {
     this.strokeOn = !this.strokeOn;
@@ -26,28 +26,51 @@ let settings = {
   },
   changeNRange: function (n) {
     this.nRange = n;
-    frameRate(getFrameRateFor(this.nRange));
+    this.framerate = getFramerate(n);
     activateButton(`n${n}`);
     setOtherButtonsInactive(n);
     initField();
   },
   changeFr: function (v) {
-    this.frameRate = this.frameRate + v > 0 ? this.frameRate + v : this.frameRate;
-    frameRate(this.frameRate);
+    this.framerate = this.framerate + v > 0 ? this.framerate + v : this.framerate;
   },
   changeSize: function (v) {
-    this.size = Math.min(Math.max(this.size * v, 16), 128);
+    this.size = Math.min(Math.max(this.size * v, 16), 1024);
     setFieldSize(this.size);
     calcBlockSize();
     initField();
+  },
+  getMil: function () {
+    return 1000 / this.framerate;
   }
 }
 
-function getFrameRateFor(n) {
+function setup() {
+  initCanvas();
+  calcBlockSize();
+  setFieldSize(settings.size);
+  initWorker();
+  initField();
+}
+
+function initWorker() {
+  worker = new Worker('calculations.js');
+  worker.onmessage = function (e) {
+    const changes = e.data;
+    changes.forEach(c => {
+      state.field[c.x][c.y].alive = c.change;
+      state.field[c.x][c.y].nn = c.nn;
+    })
+  }
+}
+
+
+
+function getFramerate(n) {
   const nToFr = {
-    1: 12,
-    2: 4,
-    3: 2,
+    1: 15,
+    2: 10,
+    3: 5,
   }
   return nToFr[n];
 }
@@ -70,32 +93,12 @@ function setFieldSize(n) {
   document.getElementById('fieldsize').innerText = `${n}x${n} px`;
 }
 
-function calcBlockSize(){
-  state.blockSize = width / settings.size;
-}
-
-function setup() {
-  color = 0;
-  frameRate(settings.frameRate);
-  initCanvas();
-  stroke(0);
-  background(120, 120, 120);
-  calcBlockSize();
-  setFieldSize(settings.size);
-  initField();
-
-  worker = new Worker('calculations.js');
-  worker.onmessage = function (e) {
-    const changes = e.data;
-    changes.forEach(c => {
-      state.field[c.x][c.y].alive = c.change;
-      state.field[c.x][c.y].nn = c.nn;
-    })
-  }
-
+function calcBlockSize() {
+  state.blockSize = state.width / settings.size;
 }
 
 function initField() {
+  worker.terminate();
   state.field = [];
   for (let x = 0; x <= settings.size; x++) {
     state.field.push([]);
@@ -104,6 +107,8 @@ function initField() {
     }
   }
   spawnCenter();
+  initWorker();
+  worker.postMessage({ field: state.field, nRange: settings.nRange });
 }
 
 
@@ -118,58 +123,91 @@ function spawnCenter() {
   state.field[x][y].alive = true;
 }
 
-function draw() {
-  background(120, 120, 120);
-  visualizeField();
+function run() {
   worker.postMessage({ field: state.field, nRange: settings.nRange });
+  visualizeField();
 }
 
 function initCanvas() {
-  var canvasDiv = document.getElementById('p5canvas');
-  var width = Math.floor(canvasDiv.offsetWidth * 0.5);
-  canvas = createCanvas(width, width);
-  canvas.parent('p5canvas');
+  state.width = Math.floor(window.innerWidth * 0.2);
+  state.height = state.width;
+  // pixel size
+  state.blockSize = state.width / settings.size;
+
+  // get the canvas element on the page
+  state.cxt = document.getElementById('automaton-canvas');
+  state.cxt.setAttribute("width", state.width);
+  state.cxt.setAttribute("height", state.height);
+  // initialiaze the canvas
+  state.cxt = state.cxt.getContext("2d");
 }
 
+state.colorCache = new Map()
 function getColor(nn) {
-  nn *= 12;
-  const center = 128;
-  const width = 127;
-  const frequency = Math.PI * 2 / 200;
-  const range = map(nn, 0, 8, 0, 255);
+  const key = `${state.color}${nn}`;
+  if (state.colorCache.has(key)) {
+    return state.colorCache.get(key)
+  } else {
 
-  if (color == range) {
-    color = 0;
-  }
+    nn *= 16;
+    const range = Math.floor((255 / 8 * settings.nRange) * nn);
 
-  const newColor = color + nn;
-  if (newColor > range) {
-    newColor = newColor % range;
-  }
+    if (state.color == range) {
+      state.color = 0;
+    }
 
-  var r = Math.sin(frequency * newColor + settings.rgbOffsets[0]) * width + center;
-  var g = Math.sin(frequency * newColor + settings.rgbOffsets[1]) * width + center;
-  var b = Math.sin(frequency * newColor + settings.rgbOffsets[2]) * width + center;
+    let newColor = state.color + nn;
+    if (newColor > range) {
+      newColor = newColor % range;
+    }
 
-  return { r, g, b, a: 255 };
+    const center = 128;
+    const width = 127;
+    const frequency = Math.PI * 2 / 200;
+    var r = Math.sin(frequency * newColor + settings.rgbOffsets[0]) * width + center;
+    var g = Math.sin(frequency * newColor + settings.rgbOffsets[1]) * width + center;
+    var b = Math.sin(frequency * newColor + settings.rgbOffsets[2]) * width + center;
+
+    const c = { r, g, b, a: 255 };
+    state.colorCache.set(key, c)
+    return c;
+  };
 };
 
 function visualizeField() {
+  let id = '';
   for (let x = 0; x < state.field.length; x++) {
     for (let y = 0; y < state.field[x].length; y++) {
       const { alive, nn } = state.field[x][y];
       if (alive) {
+        id+='1'
         const c = getColor(nn);
-        fill(c.r, c.g, c.b, 255);
-        if (settings.strokeOn) {
-          stroke(c.r, c.g, c.b, c.a);
-        }
+        state.cxt.fillStyle = `rgb(${c.r},${c.g},${c.b})`
       } else {
-        fill(0);
-        stroke(0);
+        id+='0'
+        state.cxt.fillStyle = 'rgb(0,0,0)';
       }
-      rect(x * state.blockSize, y * state.blockSize, state.blockSize, state.blockSize);
+      state.cxt.fillRect(x * state.blockSize, y * state.blockSize, state.blockSize, state.blockSize);
     }
   }
-  color += 0.5;
+  state.color += 0.5;
+  state.uniqStates.add(id)
 }
+
+let start;
+window.onload = function () {
+  setup()
+  loop()
+}
+
+var loop = function () {
+  var delta = Date.now();
+  var deltaTime = Date.now() - delta;
+  if (deltaTime >= settings.getMil()) {
+    requestAnimationFrame(loop);
+  }
+  else {
+    run()
+    setTimeout(function () { requestAnimationFrame(loop); }, settings.getMil() - deltaTime);
+  }
+};
--- calculations.js
+++ calculations.js
@@ -1,42 +1,42 @@
 onmessage = function (e) {
-    const field = e.data.field;
-    const nRange = e.data.nRange;
-    let changes = [];
-    for (let x = 0; x < field.length; x++) {
-        for (let y = 0; y < field[x].length; y++) {
-            let nn = numNeighbours(field, x, y,nRange)
-            const before = field[x][y].alive;
-            let change = deadOrAlive(field[x][y].alive, nn)
-            if (change != before) {
-                changes.push({ x, y, change, nn });
-            }
-        }
+  const field = e.data.field;
+  const nRange = e.data.nRange;
+  let changes = [];
+  for (let x = 0; x < field.length; x++) {
+    for (let y = 0; y < field[x].length; y++) {
+      let nn = numNeighbours(field, x, y, nRange)
+      const before = field[x][y].alive;
+      let change = deadOrAlive(field[x][y].alive, nn)
+      if (change != before) {
+        changes.push({ x, y, change, nn });
+      }
     }
-    postMessage(changes);
+  }
+  postMessage(changes);
 }
 
-function numNeighbours(f, x, y,nRange) {
-    let count = 0;
-    for (let i = -nRange; i <= nRange; i++) {
-        for (let j = -nRange; j <= nRange; j++) {
-            if (i === 0 && j === 0) {
-                continue;
-            }
-            if (f[x + i] && f[x + i][y + j]) {
-                count += f[x + i][y + j].alive ? 1 : 0;
-            }
-        }
-    }
-    return count;
+function numNeighbours(f, x, y, nRange) {
+  let count = 0;
+  for (let i = -nRange; i <= nRange; i++) {
+      for (let j = -nRange; j <= nRange; j++) {
+          if (i === 0 && j === 0) {
+              continue;
+          }
+          if (f[x + i] && f[x + i][y + j]) {
+              count += f[x + i][y + j].alive ? 1 : 0;
+          }
+      }
+  }
+  return count;
 }
 
 function deadOrAlive(alive, nn) {
-    const survive = [2,3];
-    const born = [3, 4];
-    if (alive) {
-        return survive.includes(nn)
-    } else {
-        return born.includes(nn)
-    }
+  const survive = [2, 3];
+  const born = [3, 4];
+  if (alive) {
+    return survive.includes(nn)
+  } else {
+    return born.includes(nn)
+  }
 }
 
